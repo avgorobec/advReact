44db5efd0262d20e6ccfe279d36be001ed5739d3
Alekxey Gorobec <a.gorobec@forvard.me>
2020-02-07 00:38:33 +0300

    Added Form sign-in and sign-up

---
+++ client/src/components/auth/sign-in-form.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function SignInForm(props) {
+  return (
+    <div>
+      <h2>Sign In</h2>
+    </div>
+  )
+}
+
+SignInForm.propTypes = {}
+
+export default SignInForm
